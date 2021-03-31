c7516dca5c6e5a2f5ed27c41a0b4dad7461dac64
Meerim Toktorova <meritoktorova@mail.com>
2021-03-31 09:49:12 +0600

    added import Comment

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import { useState } from 'react';
+
 import './App.css';
+import Posts from './components/Posts/Posts';
+ import Comment from './components/Comment/Comment';
+
 
 function App() {
 
-  const [posts, setPosts] = useState([
-    { id: 1, title: "Example posts",body: "lorem ipsun bla bla bla."},
-    { id: 2, title: "Another posts",body: "lorem ipsun bla bla bla."},
-    { id: 3, title: "Third posts",body: "lorem ipsun bla bla bla."}
-  ])
   return (
     <div className="App">
-     
+      <Posts />
+      <Comment />
     </div>
   );
 }

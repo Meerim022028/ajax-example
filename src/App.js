7fafec59fb4761987e18dcbd4bb2e8c6c77cfda1
Meerim Toktorova <meritoktorova@mail.com>
2021-04-01 14:07:18 +0600

    Added inport Author

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-
+import { useState } from 'react';
 import './App.css';
+import FullPost from './components/FullPost/FullPost';
 import Posts from './components/Posts/Posts';
- import Comment from './components/Comment/Comment';
+import Author from './components/Author/Author';
+import Comments from './components/Comments/Comments';
 
 
 function App() {
+  const [selectedPost, setSelectedPost] = useState(0);
 
+  
   return (
     <div className="App">
-      <Posts />
-      <Comment />
+      <FullPost id={selectedPost} />
+      <Posts setSelectedPost={setSelectedPost} />
+      <Comments />
+      <Author />
+
     </div>
   );
 }
-
 export default App;

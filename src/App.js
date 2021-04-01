a6295847800e8869109c5120da0b253f1fda8d8a
Meerim Toktorova <meritoktorova@mail.com>
2021-04-01 14:23:25 +0600

    App.Js components changes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,16 @@ import { useState } from 'react';
 import './App.css';
 import FullPost from './components/FullPost/FullPost';
 import Posts from './components/Posts/Posts';
-import Author from './components/Author/Author';
-import Comments from './components/Comments/Comments';
-
 
 function App() {
   const [selectedPost, setSelectedPost] = useState(0);
 
-  
   return (
     <div className="App">
       <FullPost id={selectedPost} />
       <Posts setSelectedPost={setSelectedPost} />
-      <Comments />
-      <Author />
-
     </div>
   );
 }
+
 export default App;
